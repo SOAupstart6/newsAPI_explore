f2fe1310f26f0a8569c4c647af95887f9bc308b2
ArioChen <ario03271028@yahoo.com.tw>
2017-10-28 10:05:23 +0800

    entity

---
+++ Entity/News_Entity.rb
@@ -0,0 +1,14 @@
+module NewsPraise
+    module Entity
+        class News 
+            attritube: source,
+            attritube: sortBy,
+            attritube: author,
+            attritube: title,
+            attritube: description,
+            attritube: url,
+            attritube: urlToImage,
+            attritube: publishedAt 
+        end
+    end
+end
